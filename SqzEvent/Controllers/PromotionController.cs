ea9c5a12f6b951d1e9a84e83e41b9604a529dda5
xuyian <xuyian@XUYIAN-PC>
2017-01-04 17:46:41 +0800

    changes

---
+++ SqzEvent/Controllers/PromotionController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SqzEvent.Models;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace SqzEvent.Controllers
+{
+    public class PromotionController : Controller
+    {
+        private QuestionModels db = new QuestionModels();
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        public PromotionController()
+        {
+
+        }
+
+        public PromotionController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        public ViewResult Start()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetResult(FormCollection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                var DefaultId = Convert.ToInt32(collection["DefaultRouterId"].ToString());
+                int _userid = Convert.ToInt32(collection["Id"].ToString());
+                var user = db.SurveyedUser.SingleOrDefault(m => m.Id == _userid);
+                var userResult = user.UserResult.SingleOrDefault(m => m.QuestionId == Convert.ToInt32(collection["QuestionId"].ToString()));
+
+                if (userResult == null)
+                {
+
+                    var newResult = new UserResult();
+                    user.LastQuestion = Convert.ToInt32(collection["QuestionId"].ToString());
+                    db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+                    newResult.SurveyedUserId = user.Id;
+                    newResult.QuestionId = Convert.ToInt32(collection["QuestionId"].ToString());
+                    newResult.QuestionLibId = Convert.ToInt32(collection["QuestionLibId"].ToString());
+                    newResult.AnswerContent = collection["UserResult"].ToString();
+                    newResult.QuestionTitle = collection["QuestionTitle"].ToString();
+                    db.UserResult.Add(newResult);
+                    db.SaveChanges();
+                    return Content("ADD");
+                }
+                else
+                {
+
+                    user.LastQuestion = Convert.ToInt32(collection["QuestionId"].ToString());
+                    userResult.AnswerContent = collection["UserResult"].ToString();
+                    userResult.QuestionTitle = collection["QuestionTitle"].ToString();
+                    db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+                    db.Entry(userResult).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    return Content("MODIFY");
+                }
+            }
+            return View("NotFound");
+        }
+        [HttpPost]
+        public JsonResult EendorContinue(int _DefaultRouterId, int _UserId, int _QusetionId)
+        {
+            var user = db.SurveyedUser.SingleOrDefault(m => m.Id == _UserId);
+            var res = new JsonResult();
+            if (user == null)
+            {
+                var status = new { status = "FAIL" };
+                res.Data = status;
+                return res;
+            }
+            else
+            {
+                if (_DefaultRouterId == -1)
+                {
+                    user.SurveyedUserStatus = 1;
+                    db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+                    user.LastQuestion = _QusetionId;
+                    db.SaveChanges();
+                    var status = new { status = "END" };
+                    res.Data = status;
+                    return res;
+                }
+                else
+                {
+                    var status = new { status = "CONTINUE" };
+                    res.Data = status;
+                    return res;
+                }
+            }
+        }
+        public PartialViewResult QusetionPartial(int DefaultRouterId)
+        {
+            var question = db.Question.SingleOrDefault(m => m.Id == DefaultRouterId);
+            if (question != null)
+                return PartialView(question);
+            return PartialView("NotFound");
+        }
+        // GET: Question
+        public ActionResult Index(int? UserId, int LibId)
+        {
+            if (UserId == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                var lib = db.QuestionLib.SingleOrDefault(m => m.Id == LibId);
+                var user = db.SurveyedUser.SingleOrDefault(m => m.Id == UserId && m.QuestionLibId == LibId);
+                if (lib != null)
+                {
+                    if (user != null)
+                        return View(user);
+                }
+                else
+                {
+                    user = new SurveyedUser()
+                    {
+                        QuestionLibId = LibId,
+                        SurveyedUserStatus = 0,
+                        StartTime = DateTime.Now,
+                        SurveyedUserName = "xya", //待修改
+                        LastQuestion = lib.StartQuestionId
+                    };
+                    db.SurveyedUser.Add(user);
+                    db.SaveChanges();
+                    return View(user);
+                }
+            }
+            return View("NotFound");
+        }
+    }
+}
---
+++ SqzEvent/Models/QuestionModels.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SqzEvent.Models
+{
+    public class QuestionModels : DbContext
+    {
+
+        public virtual DbSet<Question> Question { get; set; }
+        public virtual DbSet<QuestionLib> QuestionLib { get; set; }
+        public virtual DbSet<Result> Result { get; set; }
+        public virtual DbSet<SurveyedUser> SurveyedUser { get; set; }
+        public virtual DbSet<UserResult> UserResult { get; set; }
+        public QuestionModels() : base("PromotionConnection")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Question>()
+                .HasMany(e => e.Result)
+                .WithRequired(e => e.Question)
+                .HasForeignKey(e => e.QuestionId)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder.Entity<QuestionLib>()
+                .HasMany(e => e.Question)
+                .WithRequired(e => e.QuestionLib)
+                .HasForeignKey(e => e.QuestionLibId)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder.Entity<SurveyedUser>()
+               .HasMany(e => e.UserResult)
+               .WithRequired(e => e.SurveyedUser)
+               .HasForeignKey(e => e.SurveyedUserId)
+               .WillCascadeOnDelete(true);
+        }
+    }
+    /// <summary>
+    /// 被调查用户
+    /// </summary>
+    [Table("SurveyedUser")]
+    public partial class SurveyedUser
+    {
+        public int Id { get; set; }
+        public string SurveyedUserName { get; set; }
+        public int QuestionLibId { get; set; }
+        public int SurveyedUserStatus { get; set; }
+        public DateTime StartTime { get; set; }
+        public int LastQuestion { get; set; }
+        public virtual QuestionLib QuestionLib { get; set; }
+        // 对应关系
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<UserResult> UserResult { get; set; }
+    }
+    /// <summary>
+    /// 题库
+    /// </summary>
+    [Table("QuestionLib")]
+    public partial class QuestionLib
+    {
+        public int Id { get; set; }
+        public string LibName { get; set; }
+        public int EndType { get; set; }
+        public int StartQuestionId { get; set; }
+        public string RouterUrl { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Question> Question { get; set; }
+
+    }
+    /// <summary>
+    /// 题目
+    /// </summary>
+    [Table("Question")]
+    public partial class Question
+    {
+        public int Id { get; set; }
+        public int QuestionLibId { get; set; }
+        public string QuestionTitle { get; set; }
+        public int QuestionType { get; set; }
+        public int DefaultRouter { get; set; }
+        public virtual QuestionLib QuestionLib { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Result> Result { get; set; }
+    }
+    /// <summary>
+    /// 题目结果
+    /// </summary>
+    [Table("Result")]
+    public partial class Result
+    {
+        public int Id { get; set; }
+        public int QuestionId { get; set; }
+        public string AnswerContent { get; set; }
+        public int DefaultRouter { get; set; }
+        public virtual Question Question { get; set; }
+    }
+    /// <summary>
+    /// 用户结果
+    /// </summary>
+    [Table("UserResult")]
+    public partial class UserResult
+    {
+        public int Id { get; set; }
+        public int SurveyedUserId { get; set; }
+        public int QuestionLibId { get; set; }
+        public int QuestionId { get; set; }
+        public string QuestionTitle { get; set; }
+        public string AnswerContent { get; set; }
+        public virtual SurveyedUser SurveyedUser { get; set; }
+    }
+}
