285e3933aa399c67e1ca0fd26e0b8656b0e435bd
xuyian <xuyian@XUYIAN-PC>
2017-03-21 17:49:37 +0800

    changes

---
+++ SqzEvent/Controllers/WeChatMiniController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SqzEvent.Controllers
+{
+    public class WeChatMiniController : Controller
+    {
+        // GET: WeChatMini
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetResponse()
+        {
+            return Json(new { data = "content" });
+        }
+    }
+}
