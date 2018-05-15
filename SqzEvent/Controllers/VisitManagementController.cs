87b83aa17087791bdb63be63b5d5378775dde671
myron <1207278908@qq.com>
2018-05-15 09:44:07 +0800

    22

---
+++ SqzEvent/Controllers/VisitManagementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SqzEvent.Controllers
+{
+    public class VisitManagementController : Controller
+    {
+        // GET: VisitManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
